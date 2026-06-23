6ba9808df3b68d627ff6aa58e9cf762f90d1416d
Priyanshu Rajput <priyanshurajput360@gmail.com>
2026-06-24 03:17:57 +0530

    updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,55 +1,32 @@
-const georgia11Art = [
-  "",
-  "",
-  "`7MM\"\"\"Mq.`7MM\"\"\"Mq.  `7MMF'`YMM'   `MM'   db      `7MN.   `7MF'.M\"\"\"bgd ",
-  "  MM   `MM. MM   `MM.   MM    VMA   ,V    ;MM:       MMN.    M ,MI    \"Y ",
-  "  MM   ,M9  MM   ,M9    MM     VMA ,V    ,V^MM.      M YMb   M `MMb.     ",
-  "  MMmmdM9   MMmmdM9     MM      VMMP    ,M  `MM      M  `MN. M   `YMMNq. ",
-  "  MM        MM  YM.     MM       MM     AbmmmqMA     M   `MM.M .     `MM ",
-  "  MM        MM   `Mb.   MM       MM    A'     VML    M     YMM Mb     dM ",
-  ".JMML.    .JMML. .JMM..JMML.   .JMML..AMA.   .AMMA..JML.    YM P\"Ybmmd\"  ",
-  "",
-  "",
-  "",
-  "",
-  "`7MMF'  `7MMF'`7MMF'   `7MF'",
-  "  MM      MM    MM       M  ",
-  "  MM      MM    MM       M  ",
-  "  MMmmmmmmMM    MM       M  ",
-  "  MM      MM    MM       M  ",
-  "  MM      MM    YM.     ,M  ",
-  ".JMML.  .JMML.   `bmmmmd\"'  ",
-  "",
-  "",
-  "",
-  "",
-  "`7MM\"\"\"Mq.        db        `7MMF'`7MM\"\"\"Mq.`7MMF'   `7MF'MMP\"\"MM\"\"YMM ",
-  "  MM   `MM.      ;MM:         MM    MM   `MM. MM       M  P'   MM   `7 ",
-  "  MM   ,M9      ,V^MM.        MM    MM   ,M9  MM       M       MM      ",
-  "  MMmmdM9      ,M  `MM        MM    MMmmdM9   MM       M       MM      ",
-  "  MM  YM.      AbmmmqMA       MM    MM        MM       M       MM      ",
-  "  MM   `Mb.   A'     VML (O)  MM    MM        YM.     ,M       MM      ",
-  ".JMML. .JMM..AMA.   .AMMA.Ymmm9   .JMML.       `bmmmmd\"'     .JMML.    ",
-  "",
-  "",
-].join('\n')
-
-const smallPriyanshuArt = [
-  " ___     _                   _        ",
-  "| _ \\_ _(_)_  _ __ _ _ _  __| |_ _  _ ",
-  "|  _/ '_| | || / _` | ' \\(_-< ' \\ || |",
-  "|_| |_| |_|\\_, \\__,_|_||_/__/_||_\\_,_|",
-  "           |__/                       ",
+const ascii = [
+  "                      ,,                                         ,,                  ",
+  "`7MM\"\"\"Mq.            db                                       `7MM                  ",
+  "  MM   `MM.                                                      MM                  ",
+  "  MM   ,M9 `7Mb,od8 `7MM `7M'   `MF',6\"Yb.  `7MMpMMMb.  ,pP\"Ybd  MMpMMMb.`7MM  `7MM  ",
+  "  MMmmdM9    MM' \"'   MM   VA   ,V 8)   MM    MM    MM  8I   \"   MM    MM  MM    MM  ",
+  "  MM         MM       MM    VA ,V   ,pm9MM    MM    MM  `YMMMa.  MM    MM  MM    MM  ",
+  "  MM         MM       MM     VVV   8M   MM    MM    MM  L.   I8  MM    MM  MM    MM  ",
+  ".JMML.     .JMML.   .JMML.   ,V    `Moo9'Yo..JMML  JMML.M9mmmP'.JMML  JMML.`Mbod\"YML.",
+  "                            ,V                                                       ",
+  "                         OOb\"                                                        ",
+  "                                                                                     ",
+  "                     ,,                                                              ",
+  "`7MM\"\"\"Mq.           db                        mm                                    ",
+  "  MM   `MM.                                    MM                                    ",
+  "  MM   ,M9   ,6\"Yb.`7MM `7MMpdMAo.`7MM  `7MM mmMMmm                                  ",
+  "  MMmmdM9   8)   MM  MM   MM   `Wb  MM    MM   MM                                    ",
+  "  MM  YM.    ,pm9MM  MM   MM    M8  MM    MM   MM                                    ",
+  "  MM   `Mb. 8M   MM  MM   MM   ,AP  MM    MM   MM                                    ",
+  ".JMML. .JMM.`Moo9'Yo.MM   MMbmmd'   `Mbod\"YML. `Mbmo                                 ",
+  "                  QO MP   MM                                                         ",
+  "                  `bmP  .JMML.                                                        ",
 ].join('\n')
 
 export function Header() {
   return (
     <section className="my-8">
-      <div className="hidden md:block overflow-x-auto">
-        <pre className="font-mono text-[0.65rem] leading-[1.05] text-content whitespace-pre m-0">{georgia11Art}</pre>
-      </div>
-      <div className="md:hidden overflow-x-auto">
-        <pre className="font-mono text-[0.55rem] sm:text-[0.65rem] leading-[1.05] text-content whitespace-pre m-0">{smallPriyanshuArt}</pre>
+      <div className=" overflow-x-auto">
+        <pre className="font-mono sm:text-[0.85rem] text-[0.35rem] leading-[1.05] text-content whitespace-pre m-0">{ascii}</pre>
       </div>
       <p className="text-content-muted"><i>Software engineer building infrastructure, platforms, and developer tooling</i></p>
     </section>
